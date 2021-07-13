1b656a573d5ba9c07464da1ffc0ae0a85203294e
shin <zerll9100@gmail.com>
2021-07-13 16:58:51 +0900

    세션 저장소로 DB 사용하기

---
+++ src/main/java/com/zarmar/alone/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.zarmar.alone.springboot.config.auth.dto;
+
+import com.zarmar.alone.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/com/zarmar/alone/springboot/web/IndexController.java
+++ src/main/java/com/zarmar/alone/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.zarmar.alone.springboot.web;
 
+import com.zarmar.alone.springboot.config.auth.LoginUser;
+import com.zarmar.alone.springboot.config.auth.dto.SessionUser;
 import com.zarmar.alone.springboot.service.posts.PostsService;
 import com.zarmar.alone.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findALlDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
